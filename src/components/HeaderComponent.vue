eb765e719eef0080004fa767d217fcf643152d7b
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-04-08 15:33:26 +0200

    init

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <img :src="logo" :alt="logo">
+    </header>
+</template>
+
+<script>
+import logo from "@/assets/spotify-logo.png";
+
+
+
+export default {
+    name: "HeaderComponent",
+    data(){
+        return{
+            logo: logo,
+        }
+    }
+
+}
+</script>
+
+
+<style lang= 'scss' scoped>
+
+header{
+    background-color: black;
+}
+
+</style>
